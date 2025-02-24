ab6a8e1b34f0446ab226758a0c8280c0268f8b9e
Karen Galstyan <kgalstya@c1r4s5.42yerevan.am>
2025-02-24 21:11:43 +0400

    Done module00 and module01

---
+++ module_01/ex05/main.cpp
@@ -0,0 +1,11 @@
+#include "Harl.hpp"
+
+int main()
+{
+	Harl com;
+	com.complain("DEBUG");
+	com.complain("INFO");
+	com.complain("WARNING");
+	com.complain("ERROR");
+	return(0);
+}
---
+++ module_01/ex06/main.cpp
@@ -0,0 +1,45 @@
+#include "Harl.hpp"
+
+int main(int argc, char **argv)
+{
+	if(argc != 2){
+		std::cout << "Wrong number arguments" << std::endl;
+		return(1);
+	}
+	Harl com;
+	int level = 0;
+	if(!strcmp(argv[1], "DEBUG"))
+		level = 1;
+	else if(!strcmp(argv[1], "INFO"))
+		level = 2;
+	else if(!strcmp(argv[1], "WARNING"))
+		level = 3;
+	else if(!strcmp(argv[1], "ERROR"))
+		level = 4;
+	switch(level)
+	{
+		case(1):{
+			std::cout << "[ DEBUG ]" << std::endl;
+			com.complain("DEBUG");
+			break;
+		}
+		case(2):{
+			std::cout << "[ INFO ]" << std::endl;
+			com.complain("INFO");
+			break;
+		}
+		case(3):{
+			std::cout << "[ WARNING ]" << std::endl;
+			com.complain("WARNING");
+			break;
+		}
+		case(4):{
+			std::cout << "[ ERROR ]" << std::endl;
+			com.complain("ERROR");
+			break;
+		}
+		default:
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+	return(0);
+}
